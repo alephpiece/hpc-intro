caa13f8ac791cec3583d381d0b06fba76f2a837a
one <wangan.cs@gmail.com>
2022-03-11 16:09:13 +0800

    Update par_mm to provide more levels of abstraction

--- perf/make.sh
+++ perf/make.sh
@@ -1,9 +1,16 @@
 CC=g++
 
-$CC mm.cxx -o mm.exe
-$CC -O3 -fopt-info-vec mm.cxx -o vec_mm.exe
-$CC -fopenmp par_mm.cxx -o par_mm.exe
-$CC -fopenmp -O3 -fopt-info-vec par_mm.cxx -o par_vec_mm.exe
+echo "Compiling mm.cxx"
+$CC -march=native mm.cxx -o mm.exe
 
-CC=hipcc
-$CC simt_mm.cxx -o simt_mm
+echo "Compiling mm.cxx with vectorization"
+$CC -march=native -O3 -fopt-info-vec mm.cxx -o vec_mm.exe
+
+echo "Compiling par_mm.cxx with multithreading"
+$CC -march=native -fopenmp par_mm.cxx -o par_mm.exe
+
+echo "Compiling par_mm.cxx with multithreading & vectorization"
+$CC -march=native -fopenmp -O3 -fopt-info-vec par_mm.cxx -o par_vec_mm.exe
+
+echo "Compiling simt_mm.cxx with SIMT (HIP)"
+hipcc simt_mm.cxx -o simt_mm
--- perf/par_mm.cxx
+++ perf/par_mm.cxx
@@ -1,4 +1,5 @@
 #include <omp.h>
+#include <chrono> // timer
 #include <cstdio>
 #include <ctime>
 
@@ -7,37 +8,53 @@
 #define A(i, j) A[i + N * j]
 #define B(i, j) B[i + N * j]
 
+void par_mm(double *__restrict__ C,
+            double *__restrict__ A,
+            double *__restrict__ B,
+            int N, int nthreads)
+{
+  using namespace std::chrono;
+  using Timer = high_resolution_clock;
+
+  // Set the number of threads
+  omp_set_num_threads(nthreads);
+
+  auto start_time = Timer::now(); // record the start time
+
+#pragma omp parallel for
+  for (int j = 0; j < N; j++)
+    for (int k = 0; k < N; k++)
+      for (int i = 0; i < N; i++)
+        C(i, j) += A(i, k) * B(k, j);
+
+  auto stop_time = Timer::now(); // record the stop time
+  auto diff_time = duration_cast<milliseconds>(stop_time - start_time);
+
+  printf("[%d threads] JKI, N = %d, Execution time = %ld ms\n",
+         nthreads, N, diff_time.count());
+}
+
 int main() {
 
-    clock_t clocks;
+  int nthreads;
+  int N = 4000;
+  auto A = new double[N * N];
+  auto B = new double[N * N];
+  auto C = new double[N * N];
 
-    int N = 4000;
-    auto A = new double[N * N];
-    auto B = new double[N * N];
-    auto C = new double[N * N];
-    int strip = 8;
+  nthreads = 16; // Number of threads = 16
+  par_mm(C, A, B, N, nthreads);
 
-    // Set the number of threads
-    int nthreads = 4;
-    omp_set_num_threads(nthreads);
+  nthreads = 8; // Number of threads = 8
+  par_mm(C, A, B, N, nthreads);
 
-    // Loop nest: JKI
-    clocks = clock();
-#pragma omp parallel for
-    for (int j = 0; j < N; j++)
-        for (int k = 0; k < N; k++)
-            for (int i = 0; i < N; i++)
-                C(i, j) += A(i, k) * B(k, j);
+  nthreads = 4; // Number of threads = 4
+  par_mm(C, A, B, N, nthreads);
 
-    clocks = clock() - clocks; // timing
+  // cleanup
+  delete[] A;
+  delete[] B;
+  delete[] C;
 
-    printf("[%d threads] JKI, N = %d, Execution time = %ld ms\n",
-           nthreads, N, clocks * 1000 / CLOCKS_PER_SEC / nthreads);
-
-    // cleanup
-    delete[] A;
-    delete[] B;
-    delete[] C;
-
-    return 0;
+  return 0;
 }
